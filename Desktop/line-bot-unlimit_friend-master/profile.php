98a5bdad5fe38d42f31baa9b5ad278153e142106
FEMUCK <FEMUCK@users.noreply.github.com>
2019-01-14 11:15:53 +0700

    Update profile.php

--- Desktop/line-bot-unlimit_friend-master/profile.php
+++ Desktop/line-bot-unlimit_friend-master/profile.php
@@ -1,9 +1,9 @@
 <?php
 
 
-$access_token = '*Channel_Access_Token*';
+$access_token = 'M9XHo1P8RDkn/q4Yvj0xPDlpuPgRYgREWgPclCgU8cU2LF1ha9ARcG/NBuTFGQiD1gaWaXGMDA2JLjE3fOxbyaNMRdGIXasUri+C5e2guaLHFgS/lqp8EIKNkWQrxq7Ch7obQpbhGQmrQpZhPVuvpgdB04t89/1O/w1cDnyilFU=';
 
-$userId = '*User_ID*';
+$userId = 'U3ed1a41796172df16b4062a32b874909';
 
 $url = 'https://api.line.me/v2/bot/profile/'.$userId;
 
